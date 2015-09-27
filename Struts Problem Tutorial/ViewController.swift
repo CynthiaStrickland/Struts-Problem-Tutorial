346b2b02babcb75f5033c9535e34c7860df31f1c
Cynthia Whitlatch <cawhitlatch2@gmail.com>
2015-09-26 22:37:23 -0700

    Initial Commit

---
+++ Struts Problem Tutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Struts Problem Tutorial
+//
+//  Created by Cynthia Whitlatch on 9/26/15.
+//  Copyright © 2015 Cynthia Whitlatch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
